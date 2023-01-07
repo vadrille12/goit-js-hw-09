68d0c12f4596dfc0cccb36a56d63696dffe9a032
vadrille12 <109307947+vadrille12@users.noreply.github.com>
2023-01-07 18:02:52 +0000

    Deploying to gh-pages from @ vadrille12/goit-js-hw-09@0a1bacf9f54377eae76d409a79af1f727ce4fa34 🚀

---
+++ 01-color-switcher.5694cb65.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.5694cb65.js.map
